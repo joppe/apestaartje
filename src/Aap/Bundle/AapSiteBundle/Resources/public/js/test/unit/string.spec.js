cb3a61856d3886c1a441223ccb5b8b60ad3be8fc
Joppe Aarts <joppe@zicht.nl>
2015-02-13 14:12:15 +0100

    lib/lang/string jasmine test created

---
+++ src/Aap/Bundle/AapSiteBundle/Resources/public/js/test/unit/string.spec.js
@@ -0,0 +1,29 @@
+/*global define, describe, jasmine, it, expect*/
+
+define(
+    ['lib/lang/string'],
+    function (Str) {
+        'use strict';
+
+        describe('string.js', function () {
+
+            it('trim', function () {
+                expect(Str.trim('  foo  ')).toEqual('foo');
+            });
+
+            it('snake to camelcase', function () {
+                expect(Str.snakeToCamelCase('snake_styled_var_name')).toEqual('snakeStyledVarName');
+            });
+
+            it('spine to camelcase', function () {
+                expect(Str.spineToCamelCase('spine-styled-var-name')).toEqual('spineStyledVarName');
+            });
+
+            it ('ucfirst', function () {
+                expect(Str.ucfirst('are you sure?')).toEqual('Are you sure?');
+            });
+
+        });
+
+    }
+);
