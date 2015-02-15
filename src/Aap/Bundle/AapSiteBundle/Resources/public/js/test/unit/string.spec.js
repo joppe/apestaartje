7ed574f42787d64e51f9c100cfd2e49df6ebf3ec
Joppe Aarts <joppe@apestaartje.info>
2015-02-15 15:50:16 +0100

    use toBe instead of toEqueal when === comparison is required

--- src/Aap/Bundle/AapSiteBundle/Resources/public/js/test/unit/string.spec.js
+++ src/Aap/Bundle/AapSiteBundle/Resources/public/js/test/unit/string.spec.js
@@ -13,19 +13,19 @@ define(
         describe('string tests', function () {
 
             it('trim', function () {
-                expect(Str.trim('  foo  ')).toEqual('foo');
+                expect(Str.trim('  foo  ')).toBe('foo');
             });
 
             it('snake to camelcase', function () {
-                expect(Str.snakeToCamelCase('snake_styled_var_name')).toEqual('snakeStyledVarName');
+                expect(Str.snakeToCamelCase('snake_styled_var_name')).toBe('snakeStyledVarName');
             });
 
             it('spine to camelcase', function () {
-                expect(Str.spineToCamelCase('spine-styled-var-name')).toEqual('spineStyledVarName');
+                expect(Str.spineToCamelCase('spine-styled-var-name')).toBe('spineStyledVarName');
             });
 
             it ('ucfirst', function () {
-                expect(Str.ucfirst('are you sure?')).toEqual('Are you sure?');
+                expect(Str.ucfirst('are you sure?')).toBe('Are you sure?');
             });
 
         });
