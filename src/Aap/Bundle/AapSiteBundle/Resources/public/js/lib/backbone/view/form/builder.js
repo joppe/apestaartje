ed5c4187dd43ac0e4fa7b2103748b8bec1a21c4b
Joppe Aarts <joppe@apestaartje.info>
2015-02-15 21:19:54 +0100

    formbuilder

--- src/Aap/Bundle/AapSiteBundle/Resources/public/js/lib/backbone/view/form/builder.js
+++ src/Aap/Bundle/AapSiteBundle/Resources/public/js/lib/backbone/view/form/builder.js
@@ -6,23 +6,76 @@
  */
 
 define(
-    ['require', 'lib/backbone/view/abstract'],
-    function (require, AbstractView) {
+    [
+        'require',
+        'underscore',
+        'lib/lang/class',
+        'lib/backbone/view/abstract'
+    ],
+    function (
+        require,
+        _,
+        Class,
+        AbstractView
+    ) {
         'use strict';
 
         var FormBuilder;
 
         FormBuilder = AbstractView.extend({
+            className: 'form',
+
             initialize: function () {
+                Class.callSuper(AbstractView, 'initialize', arguments, this);
+
                 this.fields = {};
             },
 
-            add: function (identifier, type, options) {
+            /**
+             * Add a check if in path is no slash prefix path with 'lib/backbone/view/form/field/'
+             *
+             * @param {string} identifier
+             * @param {string} path
+             * @param {Object} options
+             * @returns {FormBuilder}
+             */
+            add: function (identifier, path, options) {
+                options.model = this.model;
+
+                this.fields[identifier] = {
+                    path: path,
+                    options: options
+                };
+
                 return this;
             },
 
+            /**
+             * The problem is that the files with the type definitions must be loaded by requirejs. This is a
+             * asynchronous process.
+             *
+             * @returns {FormBuilder}
+             */
             render: function () {
+                var identifiers = _.keys(this.fields),
+                    paths = _.map(identifiers, function (identifier) {
+                        return this.fields[identifier].path;
+                    }, this);
 
+                require(paths, _.bind(function () {
+                    var fieldClasses = arguments;
+
+                    _.each(identifiers, _.bind(function (identifier, index) {
+                        var FieldClass = fieldClasses[index],
+                            field = new FieldClass(this.fields[identifier].options);
+
+                        this.addChild(field);
+                        this.$el.append(field.render().el);
+
+                    }, this));
+                }, this));
+
+                return this;
             }
         });
 
