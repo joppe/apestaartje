bd40900a5c06b9beb785bca6978e93203301e691
Joppe Aarts <joppe@apestaartje.info>
2015-02-15 15:10:43 +0100

    function spec tests

--- src/Aap/Bundle/AapSiteBundle/Resources/public/js/test/unit/function.spec.js
+++ src/Aap/Bundle/AapSiteBundle/Resources/public/js/test/unit/function.spec.js
@@ -12,8 +12,23 @@ define(
 
         describe('function tests', function () {
 
-            it('argumentNames', function () {
+            it('get argument names', function () {
                 expect(Func.argumentNames(function (a, b, c, foo, bar) {})).toEqual(['a', 'b', 'c', 'foo', 'bar']);
+            });
+
+            it('get no argument names, when none defined', function () {
+                expect(Func.argumentNames(function () {})).toEqual([]);
+            });
+
+            it('cache function', function () {
+                var h = Func.cache(function helper() {
+                        return (new Date()).getTime();
+                    }),
+                    t = h();
+
+                setTimeout(function () {
+                    expect(h()).toEqual(t);
+                }, 200);
             });
 
         });
