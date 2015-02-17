e81958fe3417557269f021ed06aecad50a5256fa
Joppe Aarts <joppe@apestaartje.info>
2015-02-17 17:06:04 +0100

    template cacher added

---
+++ src/Aap/Bundle/AapSiteBundle/Resources/public/js/lib/backbone/view/template.js
@@ -0,0 +1,33 @@
+/*global define, window*/
+
+/**
+ * @author Joppe Aarts <joppe@apestaartje.info>
+ * @copyright Apestaartje <http://apestaartje.info>
+ */
+
+define(
+    ['underscore'],
+    function (_) {
+        'use strict';
+
+        var templates = {};
+
+        return {
+            /**
+             * @param {string} id
+             * @param {string} [html]
+             * @returns {Function}
+             */
+            get: function (id, html) {
+                var template;
+
+                if (undefined === templates[id]) {
+                    template = _.template(html);
+                    templates[id] = template;
+                }
+
+                return template;
+            }
+        };
+    }
+);
