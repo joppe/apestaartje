fd4e4ae9d72f0f910a251b5b3b06a38f8ba52f05
Joppe Aarts <joppe@apestaartje.info>
2015-04-19 12:04:24 +0200

    LL1Lexer tests added

---
+++ src/Aap/Bundle/AapSiteBundle/Resources/es6/test/unit/lib/parsing/ll1-lexer.spec.js
@@ -0,0 +1,24 @@
+/*global describe, it, expect*/
+
+import {LL1Lexer} from 'lib/parsing/ll1-lexer';
+
+describe('LL1Lexer', function () {
+    'use strict';
+
+    it('match', function () {
+        let l = new LL1Lexer('abcd');
+
+        l.match('a');
+
+        expect(l.p).toBe(1);
+        expect(l.c).toBe('b');
+    });
+
+    it('no match throws an exception', function () {
+        let l = new LL1Lexer('abcd');
+
+        expect(function () {
+            l.match('c');
+        }).toThrow();
+    });
+});
