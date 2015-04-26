5b4de218b53bf5307d30c0a7e6bac8766e76311a
Joppe Aarts <joppe@apestaartje.info>
2015-04-26 16:51:07 +0200

    systemjs config: jquery added to map and meta config added to define the backbone dependencies

--- src/Aap/Bundle/AapSiteBundle/Resources/public/js/system.conf.js
+++ src/Aap/Bundle/AapSiteBundle/Resources/public/js/system.conf.js
@@ -5,12 +5,20 @@ System.config({
 
     paths: {
         'lib/*': 'js/lib/*.es6',
+        'bundles/*': 'js/bundles/*.es6',
         'parsing/*': 'js/parsing/*.es6'
     },
 
     map: {
+        jquery: 'vendor/jquery/dist/jquery.js',
         underscore: 'vendor/underscore/underscore',
         backbone: 'vendor/backbone/backbone.js'
+    },
+
+    meta: {
+        backbone: {
+            deps: ['jquery', 'underscore']
+        }
     },
 
     transpiler: 'babel'
