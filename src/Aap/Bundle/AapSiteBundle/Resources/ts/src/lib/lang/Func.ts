a80d46701be87f9b20aee3b1ac12f37a083c4402
Joppe Aarts <joppe@apestaartje.info>
2016-05-21 13:20:17 +0200

    Func class: added execution limit

--- src/Aap/Bundle/AapSiteBundle/Resources/ts/src/lib/lang/Func.ts
+++ src/Aap/Bundle/AapSiteBundle/Resources/ts/src/lib/lang/Func.ts
@@ -3,20 +3,75 @@
  * @copyright Apestaartje <http://apestaartje.info>
  */
 
-const RE_ARGS = /^function[^\(]*\(([^\)]*)\)/m;
+/**
+ * Regular expression to get the argument names of a string representation of a function. The arguments must be wrapped
+ * in braces. Supports old fashioned functions and the fat arrow syntax.
+ *
+ * @type {RegExp}
+ */
+const RE_FUNCTION_ARGS = /^(function)?[^\(]*\(([^\)]*)\)/m;
 
 /**
- * This is a wrapper class for a function, it can be used to fetch all the names of the arguments
+ * This is a wrapper class for a function, it can be used to fetch all the names of the arguments.
  */
 export class Func {
+    /**
+     * The wrapped function.
+     *
+     * @type {Function}
+     */
     private func:Function;
 
-    constructor(func:Function) {
+    /**
+     * The maximum number of times the function can be executed. Use -1 for no limit.
+     *
+     * @type {number}
+     */
+    private executionLimit:number;
+
+    /**
+     * The number of times the function is executed.
+     *
+     * @type {number}
+     */
+    private executionCount:number = 0;
+
+    /**
+     * The result of the last execution.
+     *
+     * @type {*}
+     */
+    private lastResult:any;
+
+    /**
+     * The cache of the names of the arguments.
+     *
+     * @type {Array}
+     */
+    private args:string[];
+
+    /**
+     * @param {Function} func the function to wrap
+     * @param {number} [executionLimit] the maximum number of times the function can be executed. Use -1 for no limit.
+     */
+    constructor(func:Function, executionLimit:number = -1) {
         this.func = func;
+        this.executionLimit = executionLimit;
     }
-    
+
+    /**
+     * Execute the function and return the result.
+     *
+     * @param args
+     * @returns {any}
+     */
     invoke(...args:any[]):any {
-        return this.func(...args);
+        if (-1 === this.executionLimit || this.executionCount < this.executionLimit) {
+            this.lastResult = this.func(...args);
+            this.executionCount += 1;
+        }
+
+        return this.lastResult;
     }
 
     /**
@@ -25,18 +80,22 @@ export class Func {
      * @returns {string[]}
      */
     get argumentNames():string[] {
-        let str:string = this.func.toString(),
-            matches:string[] = str.match(RE_ARGS),
-            args:string[] = [];
+        if (undefined === this.args) {
+            let str:string = this.func.toString(),
+                matches:RegExpMatchArray = str.match(RE_FUNCTION_ARGS),
+                group:number = 2;
 
-        if (0 < matches.length && '' !== matches[1].trim()) {
-            let names:string[] = matches[1].split(',');
+            this.args = [];
 
-            args = names.map(function (arg) {
-                return arg.trim();
-            });
+            if (group <= matches.length && '' !== matches[group].trim()) {
+                let names:string[] = matches[group].split(',');
+
+                this.args = names.map(function (arg) {
+                    return arg.trim();
+                });
+            }
         }
 
-        return args;
+        return this.args;
     }
 }
--- src/Aap/Bundle/AapSiteBundle/Resources/ts/test/unit/lib/lang/Func.spec.ts
+++ src/Aap/Bundle/AapSiteBundle/Resources/ts/test/unit/lib/lang/Func.spec.ts
@@ -2,35 +2,107 @@
 
 import {Func} from 'src/lib/lang/Func';
 
-describe('Func', () => {
+describe('Func.invoke', () => {
+    it('Do not execute more then the given limit', () => {
+        let f = new Func((() => {
+            let c = 0;
+
+            return () => {
+                c += 1;
+                return c;
+            };
+        })(), 2);
+
+        expect(f.invoke()).toBe(1);
+        expect(f.invoke()).toBe(2);
+        expect(f.invoke()).toBe(2);
+    });
+
+    it('Do not execute when the limit is 0', () => {
+        let f = new Func((() => {
+            let c = 0;
+
+            return () => {
+                c += 1;
+                return c;
+            };
+        })(), 0);
+
+        expect(f.invoke()).toBe(undefined);
+        expect(f.invoke()).toBe(undefined);
+        expect(f.invoke()).toBe(undefined);
+    });
+
+    it('Execute for ever when the limit is -1', () => {
+        let f = new Func((() => {
+            let c = 0;
+
+            return () => {
+                c += 1;
+                return c;
+            };
+        })(), -1);
+
+        expect(f.invoke()).toBe(1);
+        expect(f.invoke()).toBe(2);
+        expect(f.invoke()).toBe(3);
+        expect(f.invoke()).toBe(4);
+        expect(f.invoke()).toBe(5);
+        expect(f.invoke()).toBe(6);
+    });
+
+    it('The default limit is -1', () => {
+        let f = new Func((() => {
+            let c = 0;
+
+            return () => {
+                c += 1;
+                return c;
+            };
+        })());
+
+        expect(f.invoke()).toBe(1);
+        expect(f.invoke()).toBe(2);
+        expect(f.invoke()).toBe(3);
+        expect(f.invoke()).toBe(4);
+        expect(f.invoke()).toBe(5);
+        expect(f.invoke()).toBe(6);
+    });
+});
+
+describe('Func.argumentNames', () => {
     it('Get the names of all arguments of an anonymous function', () => {
-        let f = new Func(function (a, ab, absdef) {});
+        let f = new Func(function (a, ab, absdef) {}),
+            ff = new Func((a, ab, absdef) => {});
+
+        expect(f.argumentNames).toEqual(['a', 'ab', 'absdef']);
+        expect(ff.argumentNames).toEqual(['a', 'ab', 'absdef']);
+    });
+
+    it('Get the names of all arguments of a named function', () => {
+        let f = new Func(function foo(a, ab, absdef) {});
 
         expect(f.argumentNames).toEqual(['a', 'ab', 'absdef']);
     });
-    /*/
-    it('Get the names of all arguments of a named function', function () {
-      
-        expect(Func.argumentNames(function foo(a, ab, absdef) {
-        })).toEqual(['a', 'ab', 'absdef']);
+
+    it('Get the names of all arguments of a named function, ignoring trailing spaces', () => {
+        let f = new Func(function foo (a  , ab  , absdef  ) {});
+
+        expect(f.argumentNames).toEqual(['a', 'ab', 'absdef']);
       
     });
 
-    it('Get the names of all arguments of a named function, ignoring trailing spaces', function () {
-      
-        expect(Func.argumentNames(function foo (a, ab, absdef) {
-        })).toEqual(['a', 'ab', 'absdef']);
-      
+    it('Get an empty array when no arguments for an anonymous function', () => {
+        let f = new Func(function () {}),
+            ff = new Func(() => {});
+
+        expect(f.argumentNames).toEqual([]);
+        expect(ff.argumentNames).toEqual([]);
     });
 
-    it('Get an empty array when no arguments for an anonymous function', function () {
-        expect(Func.argumentNames(function () {
-        })).toEqual([]);
-    });
+    it('Get an empty array when no arguments for a named function', () => {
+        let f = new Func(function foo () {});
 
-    it('Get an empty array when no arguments for a named function', function () {
-        expect(Func.argumentNames(function foo() {
-        })).toEqual([]);
+        expect(f.argumentNames).toEqual([]);
     });
-    /**/
 });
