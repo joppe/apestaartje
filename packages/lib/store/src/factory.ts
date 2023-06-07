56cceab73a69acd991d5110473f44274ff0798d3
joppe <joppe@apestaartje.info>
2023-06-07 20:39:53 +0200

    featstore): added store package

---
+++ packages/lib/store/src/factory.ts
@@ -0,0 +1,15 @@
+import { Store } from './Store';
+import { StoreFactory } from './StoreFactory';
+
+export const factory: StoreFactory = ((): StoreFactory => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let store: Store<any>;
+
+  return <T extends object>(initial: T): Store<T> => {
+    if (store === undefined) {
+      store = new Store<T>(initial);
+    }
+
+    return store;
+  };
+})();
