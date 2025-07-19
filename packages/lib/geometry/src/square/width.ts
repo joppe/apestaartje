352905b4465bafbe68a62ace1423e9609397b758
joppe <joppe@apestaartje.info>
2025-07-19 12:21:14 +0200

    chore: rename

--- packages/lib/geometry/src/square/width.ts
+++
@@ -1,8 +0,0 @@
-import type { Square } from './Square';
-
-/**
- * Calculate the width of a square
- */
-export function width(square: Square): number {
-  return Math.abs(square.bottomRight.x - square.topLeft.x);
-}
