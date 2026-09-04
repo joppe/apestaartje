72fffe49c1d49fcd04bab7f2731202f0624ba252
joppe <joppe@apestaartje.info>
2026-09-04 15:01:42 +0200

    chore(): removed comment

--- packages/lib/geometry/src/vector/dotProduct.ts
+++ packages/lib/geometry/src/vector/dotProduct.ts
@@ -1,9 +1,5 @@
 import type { Vector } from './Vector';
 
-/**
- * Multiply a vectors with a value
- */
-
 export function dotProduct(a: Vector, b: Vector): number {
   return a.x * b.x + a.y * b.y;
 }
